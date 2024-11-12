ef04ec3b705a936c50d93f3d8d9f21413dedaf83
jureuk7 <jureuk7@protonmail.com>
2024-11-13 04:05:38 +0900

    Refactor ecosystem.config.js to update script path in apps configuration

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -2,11 +2,7 @@ module.exports = {
   apps: [
     {
       name: 'app',
-      // 아래 경로 중 실제 진입점 파일 경로로 수정
-      script: './dist/src/index.js', // 또는
-      // script: './dist/src/app.js', // 또는
-      // script: './dist/server.js',  // 또는
-      // script: './dist/main.js',
+      script: './dist/main.js',
       instances: 1,
       exec_mode: 'cluster',
       env: {
