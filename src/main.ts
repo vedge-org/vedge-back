adbbf3abf53a2fc293ab025e6182987a757d619e
jureuk7 <jureuk7@protonmail.com>
2024-11-15 18:08:05 +0900

    refactor: Update session cookie

--- src/main.ts
+++ src/main.ts
@@ -40,7 +40,7 @@ async function bootstrap() {
       name: 'sessionId',
       cookie: {
         httpOnly: true,
-        secure: process.env.NODE_ENV === 'production',
+        sameSite: 'lax',
         maxAge: 60 * 60 * 1000, // 1 hour
       },
     }),
